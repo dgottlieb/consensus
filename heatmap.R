6f8feb3682e075e807c8534cc986ddf12ddea885
Daniel Gottlieb <danny.gottlieb@gmail.com>
2014-07-11 10:37:58 -0400

    attempt at making heatmap.R more environment friendly

--- heatmap.R
+++ heatmap.R
@@ -1,7 +1,7 @@
 library(ggplot2)
 library(reshape2)
-lag <- as.matrix(read.csv("~/dev/go/src/github.com/dgottlieb/consensus/lag.csv"))
-p <- ggplot(melt(lag)) + geom_tile(aes(Var1, Var2, fill=value)) + 
+lag <- as.matrix(read.csv("./lag.csv"))
+p <- ggplot(melt(lag)) + geom_tile(aes(Var1, Var2, fill=value)) +
                          scale_fill_gradient(low = "white", high = "steelblue", name="lag (seconds)") +
                          xlab("") + ylab("")
-ggsave(filename="lag.png", p, path="~/dev/go/src/github.com/dgottlieb/consensus/img")
+ggsave(filename="lag.png", p, path="./img")
